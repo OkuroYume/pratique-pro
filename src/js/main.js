314cec847ac35ce6e6406af86955f20fd33412c4
OkuroYume <yuma.comazzi@studentfr.ch>
2024-06-12 12:14:19 +0200

    working on my burger menu

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+function toggleMenu(event) {
+  event.stopPropagation();
+  var menuItems = document.getElementById("menuItems");
+  var burgerMenu = document.querySelector(".burger-menu");
+
+  menuItems.classList.toggle("show");
+  burgerMenu.classList.toggle("change");
+}
+
+document.querySelector(".burger-menu").addEventListener("click", toggleMenu);
+
+document
+  .querySelector(".burger-menu")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+  });
+
+function toggleMenu(event) {
+  event.stopPropagation();
+  var menuItems = document.getElementById("menuItems");
+  var burgerMenu = document.querySelector(".burger-menu");
+  var hideOnMenu = document.getElementById("hideOnMenu");
+  var hideLogo = document.getElementById("hideLogo");
+
+  menuItems.classList.toggle("show");
+  burgerMenu.classList.toggle("change");
+  hideOnMenu.classList.toggle("hide");
+  hideLogo.classList.toggle("hide");
+}
+
+document
+  .querySelector(".burger-menu")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    toggleMenu(event);
+  });
