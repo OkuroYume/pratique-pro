f19db6aa80ae83a4b1254faf034326b0fd400063
OkuroYume <yuma.comazzi@studentfr.ch>
2024-06-12 17:10:22 +0200

    burger menu + header fully done

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,21 @@ function toggleMenu(event) {
   var burgerMenu = document.querySelector(".burger-menu");
   var hideOnMenu = document.getElementById("hideOnMenu");
   var hideLogo = document.getElementById("hideLogo");
+  var header = document.getElementById("header");
 
   menuItems.classList.toggle("show");
   burgerMenu.classList.toggle("change");
   hideOnMenu.classList.toggle("hide");
   hideLogo.classList.toggle("hide");
+  header.classList.toggle("header-blur");
 }
 
 document.querySelector(".burger-menu").addEventListener("click", toggleMenu);
+
+var menuItems = document.querySelectorAll(".menu-items a");
+
+menuItems.forEach(function (menuItem) {
+  menuItem.addEventListener("click", function () {
+    toggleMenu();
+  });
+});
