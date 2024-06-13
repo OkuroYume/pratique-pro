7d0fff812421966e0e85e98b6746dc61a37fd0e7
OkuroYume <yuma.comazzi@studentfr.ch>
2024-06-13 16:36:20 +0200

    shop almost done

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,43 @@ menuItems.forEach(function (menuItem) {
     toggleMenu();
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var products = [
+    { imageId: "productImage1", infoId: "productInfo1" },
+    { imageId: "productImage2", infoId: "productInfo2" },
+    { imageId: "productImage3", infoId: "productInfo3" },
+  ];
+
+  var currentProductIndex = 0;
+
+  function showProduct(index) {
+    products.forEach((product, idx) => {
+      const productImage = document.getElementById(product.imageId);
+      const productInfo = document.getElementById(product.infoId);
+
+      const isActive = idx === index;
+      productImage.classList.toggle("active", isActive);
+      productInfo.classList.toggle("active", isActive);
+      const glutenFreeSymbol = document.querySelector(".gluten-free-symbol");
+      if (product.imageId === "productImage2") {
+        glutenFreeSymbol.style.display = isActive ? "block" : "none";
+      }
+    });
+  }
+
+  document.getElementById("prevProduct").addEventListener("click", function () {
+    currentProductIndex =
+      currentProductIndex > 0 ? currentProductIndex - 1 : products.length - 1;
+    showProduct(currentProductIndex);
+  });
+
+  document.getElementById("nextProduct").addEventListener("click", function () {
+    currentProductIndex =
+      currentProductIndex < products.length - 1 ? currentProductIndex + 1 : 0;
+    showProduct(currentProductIndex);
+  });
+
+  // Initial display
+  showProduct(currentProductIndex);
+});
